0539cd916182724585caa5517e7f31a60e8d9f00
Gautam Singh <gautamsingh68211@gmail.com>
2026-04-28 17:54:59 +0530

    Merged 'gautam' branch with 'main' on commit 'Integrated the sockets on auction page and changed the auction page layout'

--- src/shared/services/socket.js
+++ src/shared/services/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const socket = io(SOCKET_URL, {
-    autoConnect: false,
+    autoConnect: true,
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
